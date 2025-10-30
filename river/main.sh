d6bcb0e8abeff4c9ea7ee285df0a204ea56c2b2d
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-10-30 19:12:44 +0700

    feat: support pixi

--- river/main.sh
+++ river/main.sh
@@ -1,8 +1,5 @@
 cd analysis
 
-# Download the database files
-# wget https://zenodo.org/records/10394426/files/CircNetVis_resources.tar.gz?download=1 -O CircNetVis_resources.tar.gz
-# tar -xvzf CircNetVis_resources.tar.gz -C CircNetVis --strip-components 1
 if [ -d "CircNetVis_mm9/Tools" ]; then
     echo "CircNetVis_mm9 resources already exist."
 else
@@ -10,4 +7,5 @@ else
     tar -xvzf CircNetVis_mm9_resources.tar.gz -C CircNetVis_mm9 --strip-components 1
 fi
 # Run the Shiny app
-pixi run Rscript -e "shiny::runApp('CircNetVis_mm9')"
+eval "$(pixi shell-hook)"
+Rscript -e "shiny::runApp('CircNetVis_mm9')"
